ae3868b2453f8ce0da262408d83be81d2bb0479e
zawakasutera <zawazawanomorichan@outlook.jp>
2022-07-31 01:03:39 +0900

    fix #3

--- docs/math/divisor.md
+++ docs/math/divisor.md
@@ -1,19 +1,19 @@
 ---
-title: Divisor
+title: Divisor(約数列挙)
 documentation_of: //src/math/divisor.hpp
 ---
 
-## 関数
+## 概要
 ```
 std::vector<int> divisor(int x)
 ```
 
-
 試し割りを用いた素朴な約数列挙
 
 
-### 制約
-- $x \le 10^9$
+### 機能
+* `divisor(int x)`: xを約数列挙します。xは32ビットに収まる0以上の整数である必要があります。
+
 
 ### 計算量
 - $O(\sqrt(x))$ 
--- src/math/divisor.hpp
+++ src/math/divisor.hpp
@@ -8,14 +8,14 @@ namespace zawa {
     std::vector<int> divisor(int x) {
         std::vector<int> res;
 
-        for (int i = 1 ; i*i <= x ; i++) {
+        for (int i = 1 ; i * i <= x ; i++) {
             if (x % i) continue;
             res.emplace_back(i);
-            if (i*i != x) res.emplace_back(x / i);
+            if (i * i != x) res.emplace_back(x / i);
         }
         std::sort(res.begin(), res.end());
 
         return res;
     }
 
-}
+}// namespace zawa
