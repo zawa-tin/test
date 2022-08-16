8fea64bccf239e76434b1594997023f52457c277
zawakasutera <zawazawanomorichan@outlook.jp>
2022-08-16 15:43:51 +0900

    add input -#41

--- src/dataStructure/Accum-1D.hpp
+++ src/dataStructure/Accum-1D.hpp
@@ -13,20 +13,32 @@ namespace zawa {
     public:
         Accum_1D(const std::vector<T>& arr) 
             : container(arr.begin(), arr.end())
-            , accum(std::vector<T>(arr.size() + 1)) {
-
-                for (int i = 0 ; i < (int)arr.size() ; i++) {
-                    accum[i + 1] = accum[i] + container[i];
-                }
-        }
+            , accum(std::vector<T>(arr.size() + 1)) {}
         
         Accum_1D(const std::vector<T>& arr, T elem)
             : container(arr.begin(), arr.end())
-            , accum(std::vector<T>(arr.size() + 1, elem)) {
-                
-                for (int i = 0 ; i < (int)arr.size() ; i++) {
-                    accum[i + 1] = accum[i] + container[i];
-                }
+            , accum(std::vector<T>(arr.size() + 1, elem)) {}
+
+        Accum_1D(int n)
+            : container(n)
+            , accum(n + 1) {}
+
+        Accum_1D(int n, T elem)
+            : container(n)
+            , accum(n + 1, elem) {}
+
+        void set(int idx, T val) {
+            container[idx] = val;
+        }
+
+        void add(int idx, T val) {
+            container[idx] += val;
+        }
+
+        void build() {
+            for (int i = 0 ; i < (int)container.size() ; i++) {
+                accum[i + 1] = accum[i] + container[i];
+            }
         }
 
         T get(int idx) {
