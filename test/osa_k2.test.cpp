07e7aaed8dc11e5a0a5725db0866bff116e013a7
zawakasutera <zawazawanomorichan@outlook.jp>
2022-08-20 22:48:53 +0900

    verify #14

---
+++ test/osa_k2.test.cpp
@@ -0,0 +1,9 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include <iostream>
+#include "../src/math/osa_k.hpp"
+
+int main() {
+    // https://atcoder.jp/contests/abc170/submissions/34176723
+    std::cout << "Hello World" << std::endl;
+}
