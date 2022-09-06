99fa5c25e5c6e4c1506bb0c6be9dc52a0524375c
zawakasutera <zawazawanomorichan@outlook.jp>
2022-09-06 15:47:14 +0900

    verify and imp -#8

---
+++ test/Compression.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "../src/algorithm/Compression.hpp"
+
+#include <iostream>
+
+int main() {
+    // https://atcoder.jp/contests/abc036/submissions/34650039
+    std::cout << "Hello World" << std::endl;
+}
