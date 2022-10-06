b2febda7198b49195651fa33647dc06f84afe7fe
zawakasutera <zawazawanomorichan@outlook.jp>
2022-10-07 03:05:33 +0900

    fix verify #82

--- test/Floyd-Cycle-Detection.test.cpp
+++ test/Floyd-Cycle-Detection.test.cpp
@@ -1,75 +1,34 @@
-namespace zawa::impl {
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
 
-template <class T>
-struct Cycle_Info {
-    T start;
-    int length;
-};
-
-} // namespace zawa::impl
-
-namespace zawa {
-
-template <class T, class F>
-impl::Cycle_Info<T> floyd(T s, const F& nxt, int limit = 50000000) {
-    T turtle = s, rabbit = s;
-    int check = 0;
-    for ( ; check < limit ; check++) {
-        turtle = nxt(turtle);
-        rabbit = nxt(nxt(rabbit));
-        if (turtle == rabbit) {
-            break;
-        }
-    }
-    if (check == limit) {
-        return { s, -1 };
-    }
-    rabbit = s;
-    while (1) {
-        rabbit = nxt(rabbit);
-        turtle = nxt(turtle);
-        if (rabbit == turtle) {
-            break;
-        }
-    }
-    T start = turtle;
-    int length = 0;
-    while (1) {
-        length++;
-        turtle = nxt(turtle);
-        rabbit = nxt(nxt(rabbit));
-        if (turtle == rabbit) {
-            break;
-        }
-    }
-    return { start, length };
-}
-
-} // namespace zawa
+#include "../src/algorithm/Floyd-Cycle-Detection.hpp"
 
 #include <iostream>
 #include <vector>
 
 int main() {
-    int n;
-    std::cin >> n;
-    long long k;
-    std::cin >> k;
-    std::vector as(n, 0);
-    for (auto& a : as) {
-        std::cin >> a;
-        a--;
-    }
-    auto nxt = [&](int now) {
-        return as[now];
-    };
-    auto info = zawa::floyd(0, nxt);
-    int ans = 0;
-    while (k--) {
-        if (ans == info.start) {
-            k %= info.length;
-        }
-        ans = nxt(ans);
-    }
-    std::cout << ans + 1 << std::endl;
+    // https://atcoder.jp/contests/abc167/submissions/35438785
+
+    // int n;
+    // std::cin >> n;
+    // long long k;
+    // std::cin >> k;
+    // std::vector as(n, 0);
+    // for (auto& a : as) {
+    //     std::cin >> a;
+    //     a--;
+    // }
+    // auto nxt = [&](int now) {
+    //     return as[now];
+    // };
+    // auto info = zawa::floyd(0, nxt);
+    // int ans = 0;
+    // while (k--) {
+    //     if (ans == info.start) {
+    //         k %= info.length;
+    //     }
+    //     ans = nxt(ans);
+    // }
+    // std::cout << ans + 1 << std::endl;
+
+    std::cout << "Hello World" << std::endl;
 }
