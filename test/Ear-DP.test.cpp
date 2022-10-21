e199828da554509b704fcb66c4b0ae3ea36d2ec4
zawakasutera <zawazawanomorichan@outlook.jp>
2022-10-21 15:36:43 +0900

    re-verify

--- test/Ear-DP.test.cpp
+++ test/Ear-DP.test.cpp
@@ -1,18 +1,14 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
 
-#include <iostream>
-#include <string>
 #include "../src/algorithm/Ear-DP.hpp"
 
-int main() {
-    // https://atcoder.jp/contests/abc211/submissions/33787349
-    // std::string s;
-    // std::cin >> s;
-    // std::vector<char> as(s.size());
-    // for (int i = 0 ; i < s.size() ; i++) as[i] = s[i];
-    // std::vector<char> bs = {'c', 'h', 'o', 'k', 'u', 'd', 'a', 'i'};
-    // 
-    // std::cout << (zawa::ear_dp(as, bs)).val() << std::endl;
+#include <iostream>
 
+int main() {
     std::cout << "Hello World" << std::endl;
 }
+
+/*
+ * ABC211-C chokudai
+ * https://atcoder.jp/contests/abc211/submissions/35826346
+ */
