ede0c59bd91e8f9df949fe1999c4b0089e963cc3
zawakasutera <zawazawanomorichan@outlook.jp>
2022-12-05 12:46:25 +0900

    verify

---
+++ test/potentialized_unionfind2.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "../src/dataStructure/potentialized_unionfind.hpp"
+
+#include <iostream>
+
+int main() {
+    // int n, m; std::cin >> n >> m;
+    // zawa::potentialized_unionfind<int> uf(n);
+    // bool ans = true;
+    // for (int _ = 0 ; _ < m ; _++) {
+    //     int l, r, d;
+    //     std::cin >> l >> r >> d;
+    //     l--;
+    //     r--;
+    //     if (uf.same(l, r)) {
+    //         ans &= uf.diff(r, l) == d;
+    //     }
+    //     else {
+    //         uf.merge(l, r, d);
+    //     }
+    // }
+    // std::cout << (ans ? "Yes" : "No") << std::endl;
+    
+    std::cout << "Hello World" << std::endl;
+}
+
+/*
+ * AtCoder Beginner Contest 087 D - People on a Line
+ * https://atcoder.jp/contests/abc087/submissions/37034550
+ */
---
+++ test/potentialized_unionfind3.test.cpp
@@ -0,0 +1,50 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "../src/dataStructure/potentialized_unionfind.hpp"
+
+#include <iostream>
+
+int main() {
+    // int n, m, q; std::cin >> n >> m >> q;
+    // zawa::potentialized_unionfind<long long> uf(n);
+    // std::vector infs(n, false);
+    // for (int _ = 0 ; _ < m ; _++) {
+    //     int a, b, c; std::cin >> a >> b >> c;
+    //     a--; b--;
+    //     if (uf.same(a, b)) {
+    //         if (uf.diff(b, a) != c) {
+    //             infs[uf.leader(a)] = true;
+    //         }
+    //     }
+    //     else {
+    //         bool ng = false;
+    //         if (infs[uf.leader(a)] or infs[uf.leader(b)]) {
+    //             ng = true;
+    //         }
+    //         uf.merge(a, b, c);
+    //         if (ng) {
+    //             infs[uf.leader(a)] = true;
+    //         }
+    //     }
+    // }
+    // for (int _ = 0 ; _ < q ; _++) {
+    //     int x, y; std::cin >> x >> y;
+    //     x--; y--;
+    //     if (!uf.same(x, y)) {
+    //         std::cout << "nan" << std::endl;
+    //     }
+    //     else if (infs[uf.leader(x)]) {
+    //         std::cout << "inf" << std::endl;
+    //     }
+    //     else {
+    //         std::cout << uf.diff(y, x) << std::endl;
+    //     }
+    // }
+
+    std::cout << "Hello World" << std::endl;
+}
+
+/*
+ * AtCoder Beginner Contest 280 - F Pay or Receive
+ * https://atcoder.jp/contests/abc280/submissions/37034669
+ */
