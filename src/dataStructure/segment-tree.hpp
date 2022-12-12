4c24b7c0d79cc3b4296d15429cdadb2eb41c169f
zawakasutera <zawazawanomorichan@outlook.jp>
2022-12-12 17:22:38 +0900

    add max_right all_prod

--- src/dataStructure/segment-tree.hpp
+++ src/dataStructure/segment-tree.hpp
@@ -9,7 +9,7 @@ namespace zawa {
 template <class T, T (*op)(T, T), T (*e)()>
 class segment_tree {
 private:
-    std::size_t n;
+    int n;
     std::vector<T> dat;
 
     void update(int pos) {
@@ -21,10 +21,10 @@ private:
 
 public:
 
-    segment_tree(std::size_t n) : n(n), dat(2 * n, e()) {}
+    segment_tree(int n) : n(n), dat(2 * n, e()) {}
 
     segment_tree(const std::vector<T>& as) : n(as.size()), dat(2 * as.size(), e()) {
-        for (std::size_t i = 0 ; i < n ; i++) {
+        for (int i = 0 ; i < n ; i++) {
             dat[i + n] = as[i];
         }
         for (int i = n - 1 ; i >= 1 ; i--) {
@@ -33,22 +33,22 @@ public:
     }
 
     void set(int pos, T val) {
-        assert(0 <= pos and pos < (int)n);
-        pos += (int)n;
+        assert(0 <= pos and pos < n);
+        pos += n;
         dat[pos] = val;  
         update(pos);
     }
 
     void apply(int pos, T val) {
-        assert(0 <= pos and pos < (int)n);
-        pos += (int)n;
+        assert(0 <= pos and pos < n);
+        pos += n;
         dat[pos] = op(dat[pos], val);
         update(pos);
     }
 
     T query(int l, int r) {
-        assert(0 <= l and l < (int)n);
-        assert(l <= r and r <= (int)n);
+        assert(0 <= l and l < n);
+        assert(l <= r and r <= n);
         l += n;
         r += n;
         T ans = e();
@@ -70,23 +70,46 @@ public:
     }
 
     int max_right(int l, const std::function<bool(int)>& f) {
-        assert(0 <= l and l < (int)n);
-        assert(f(e()));
+        assert(0 <= l and l < n);
+        int r = 2 * n;
         l += n;
         T sum = e();
-        while (1) {
-            if (!f(op(sum, dat[l]))) {
-            }
-            else {
-                l++;
-                if ((l & (-l)) == l) {
-                    break;
+        auto down_tree = [&](int v) -> int {
+            while (v < n) {
+                if (f(op(sum, dat[(v << 1) | 0]))) {
+                    sum = op(sum, dat[(v << 1) | 0]);
+                    v = ((v << 1) | 1);
                 }
-                if (l >= (i32)dat.size()) {
-                    break;
+                else {
+                    v = ((v << 1) | 0);
+                }
+            }
+            return v - n;
+        };
+        int cnt = 0;
+        for ( ; l < r ; cnt++) {
+            if (l & 1) {
+                if (!f(op(sum, dat[l]))) {
+                    return down_tree(l);
+                }
+                sum = op(sum, dat[l++]);
+            }
+            r -= (r & 1);
+            l >>= 1;
+            r >>= 1;
+        }
+        while (cnt--) {
+            r <<= 1;
+            if (((r | 1) << cnt) <= 2 * n) {
+                if (!f(op(sum, dat[r]))) {
+                    return down_tree(r);
+                }
+                else {
+                    sum = op(sum, dat[r++]);
                 }
             }
         }
+        return n;
     }
 
     T debug(int pos) {
