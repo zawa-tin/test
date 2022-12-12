3e9d223518762b2bad328c1039ea93c7977ff92b
zawakasutera <zawazawanomorichan@outlook.jp>
2022-12-12 17:22:54 +0900

    add verify

---
+++ test/segment-tree4.test.cpp
@@ -0,0 +1,38 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "../src/dataStructure/segment-tree.hpp"
+
+#include <iostream>
+#include <algorithm>
+
+// long long op(long long a, long long b) {
+//     return std::max(a, b);
+// }
+// 
+// long long e() {
+//     return 0LL;
+// }
+
+int main() {
+    // int n; std::cin >> n;
+    // std::vector hs(n, 0); 
+    // for (auto& h : hs) {
+    //     std::cin >> h;
+    // }
+    // std::vector as(n, 0);
+    // for (auto& a : as) {
+    //     std::cin >> a;
+    // }
+    // zawa::segment_tree<long long, op, e> seg(n + 1);
+    // for (int i = 0 ; i < n ; i++) {
+    //     seg.set(hs[i], seg.query(0, hs[i]) + as[i]);
+    // }
+    // std::cout << seg.all_prod() << std::endl;
+
+    std::cout << "Hello World" << std::endl;
+}
+
+/*
+ * Educational DP Contest / DPまとめコンテスト Q Flowers
+ * https://atcoder.jp/contests/dp/submissions/37231045
+ */
