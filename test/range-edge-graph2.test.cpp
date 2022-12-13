fab585e2642a5a968f190078908ddb0994faca8f
zawakasutera <zawazawanomorichan@outlook.jp>
2022-12-14 06:58:06 +0900

    verify

---
+++ test/range-edge-graph2.test.cpp
@@ -0,0 +1,42 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "../src/graph/range-edge-graph.hpp"
+
+#include <iostream>
+#include <ios>
+
+int main() {
+    // std::ios::sync_with_stdio(false);
+    // std::cin.tie(nullptr);
+    // 
+    // int n, q, s; std::cin >> n >> q >> s;
+    // zawa::range_edge_graph<long long> reg(n);
+    // for (int _ = 0 ; _ < q ; _++) {
+    //     int t; std::cin >> t;
+    //     if (t == 1) {
+    //         int v, u, w; std::cin >> v >> u >> w;
+    //         reg.add_edge(v - 1, v, u - 1, u, w);
+    //     }
+    //     if (t == 2) {
+    //         int v, l, r, w; std::cin >> v >> l >> r >> w;
+    //         reg.add_edge(v - 1, v, l - 1, r, w);
+    //     }
+    //     if (t == 3) {
+    //         int v, l, r, w; std::cin >> v >> l >> r >> w;
+    //         reg.add_edge(l - 1, r, v - 1, v, w);
+    //     }
+    // }
+    // 
+    // const long long sup = (1LL << 60);
+    // auto ds = reg.dijkstra(s - 1, sup);
+    // for (int i = 0 ; i < n ; i++) {
+    //     std::cout << (ds[i] == sup ? -1LL : ds[i]) << (i + 1 == n ? '\n' : ' ');
+    // }
+
+    std::cout << "Hello World" << std::endl;
+}
+
+/*
+ * Codeforces Round #406 B. Legacy
+ * https://codeforces.com/contest/786/submission/185125317
+ */
