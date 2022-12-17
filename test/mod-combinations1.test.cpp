0665b92a05f6bd96ec02ee60135c0e0de6f4596d
zawakasutera <zawazawanomorichan@outlook.jp>
2022-12-17 09:41:42 +0900

    verify

---
+++ test/mod-combinations1.test.cpp
@@ -0,0 +1,47 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "../src/math/mod-combinations.hpp"
+
+#include <iostream>
+
+// int read() {
+//     int res = 0;
+//     while (1) {
+//         char d; std::cin >> d;
+//         if ('0' <= d and d <= '9') {
+//             res *= 10;
+//             res += (d - '0');
+//         }
+//         else {
+//             break;
+//         }
+//     }
+//     return res;
+// }
+
+int main() {
+    // zawa::mod_conbinations<1000000007LL> mc(1000000);
+    // int t; std::cin >> t;
+    // for (int _ = 0 ; _ < t ; _++) {
+    //     char c; std::cin >> c;
+    //     char __; std::cin >> __;
+    //     int n = read();
+    //     int r = read();
+    //     if (c == 'C') {
+    //         std::cout << mc.C(n, r) << std::endl;
+    //     }
+    //     if (c == 'P') {
+    //         std::cout << mc.P(n, r) << std::endl;
+    //     }
+    //     if (c == 'H') {
+    //         std::cout << mc.H(n, r) << std::endl;
+    //     }
+    // } 
+
+    std::cout << "Hello World" << std::endl;
+}
+
+/*
+ * yukicoder No.117 組み合わせの数
+ * https://yukicoder.me/submissions/826376
+ */
