40d3db9d5cc3f121a6e01778c5e51c64b5b47106
zawakasutera <zawazawanomorichan@outlook.jp>
2022-12-21 10:50:51 +0900

    verify simple-cc

---
+++ test/simple-cc1.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/11/ALDS1_11_D"
+
+#include "../src/graph/simple/connected-components.hpp"
+
+#include <cassert>
+#include <vector>
+#include <iostream>
+
+int main() {
+    int n, m; std::cin >> n >> m;
+    std::vector G1(n, std::vector(0, 0));
+    std::vector G2(n, std::vector(0, std::pair(0, 0)));
+    for (int _ = 0 ; _ < m ; _++) {
+        int s, t; std::cin >> s >> t;
+        G1[s].push_back(t);
+        G1[t].push_back(s);
+        G2[s].emplace_back(t, 0);
+        G2[t].emplace_back(s, 0);
+    }
+    zawa::connected_components cc1(G1);
+    auto cc2 = zawa::weighted_cc(G2);
+    int q; std::cin >> q;
+    for (int _ = 0 ; _ < q ; _++) {
+        int s, t; std::cin >> s >> t;
+        assert(cc1.same(s, t) == cc2.same(s, t));
+        assert(cc1.same(s, t) == (cc1[s] == cc1[t]));
+        assert(cc2.same(s, t) == (cc2[s] == cc2[t]));
+        std::cout << (cc1.same(s, t) ? "yes" : "no") << std::endl;
+    }
+}
