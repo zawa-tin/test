20f648b81d6f30cd2ceb4917891c6a83455498cb
zawa-tin <contactaizawa@icloud.com>
2023-01-15 01:17:32 +0900

    matrix verify

---
+++ test/matrix1.test.cpp
@@ -0,0 +1,35 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "../src/math/matrix.hpp"
+#include "../src/math/modint.hpp"
+
+// using mint = zawa::modint<1000000007>;
+// using mat = zawa::matrix<mint>;
+
+#include <iostream>
+
+int main() {
+	// int n; std::cin >> n;
+	// long long k; std::cin >> k;
+	// mat G(n, n);
+	// for (int i = 0 ; i < n ; i++) {
+	// 	for (int j = 0 ; j < n ; j++) {
+	// 		int p; std::cin >> p;
+	// 		G[i][j] = p;
+	// 	}
+	// }
+	// G = G.pow(k);
+	// mint ans;
+	// for (int i = 0 ; i < n ; i++) {
+	// 	for (int j = 0 ; j < n ; j++) {
+	// 		ans += G[i][j];
+	// 	}
+	// }
+	// std::cout << ans.val() << std::endl;
+	std::cout << "Hello World" << std::endl;
+}
+
+/*
+ * Educational DP Contest - R Walk
+ * https://atcoder.jp/contests/dp/submissions/38023510
+ */
