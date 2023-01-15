9016c2aad3e928805f15345243b6ef395cb4a18d
zawa-tin <contactaizawa@icloud.com>
2023-01-16 02:41:41 +0900

    182 fix monoid

--- src/utility/monoid/add.hpp
+++ src/utility/monoid/add.hpp
@@ -3,8 +3,8 @@
 namespace zawa {
 
 template <class dat_type>
-class add_monoid {
-	using T = dat_type
+struct add_monoid {
+	using T = dat_type;
 	static constexpr T id = 0;
 	static T op(const T& a, const T& b) {
 		return a + b;
--- src/utility/monoid/max.hpp
+++ src/utility/monoid/max.hpp
@@ -6,9 +6,9 @@
 namespace zawa {
 
 template <class dat_type>
-class max_monoid {
+struct max_monoid {
 	using T = dat_type;
-	static constexpr T id = std::numeric_limits<T>::max();
+	static constexpr T id = std::numeric_limits<T>::min();
 	static T op(const T& a, const T& b) {
 		return std::max(a, b);
 	}
