0f084f0a09bbebb83458155e6e4b24e78da186c7
zawa-tin <contactaizawa@icloud.com>
2023-01-16 00:45:54 +0900

    178 update ABC285D.test.cpp

--- test/ABC285D.test.cpp
+++ test/ABC285D.test.cpp
@@ -1,3 +1,5 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
 #include "../src/graph/simple/topo-sort.hpp"
 #include "../src/algorithm/compression.hpp"
 
@@ -7,20 +9,21 @@
 #include <string>
 
 int main() {
-	int n; std::cin >> n;
-	std::vector<std::string> buc;
-	std::vector<std::pair<std::string, std::string>> ps(n);
-	for (auto& [s, t] : ps) {
-		std::cin >> s >> t;
-		buc.push_back(s);
-		buc.push_back(t);
-	}
-	zawa::compression comp(buc);
-	std::vector<std::vector<int>> G(comp.size());
-	for (const auto& [s, t] : ps) {
-		G[comp[s]].emplace_back(comp[t]);
-	}
-	std::cout << (zawa::topo_sort(G).ok() ? "Yes" : "No") << std::endl;
+	// int n; std::cin >> n;
+	// std::vector<std::string> buc;
+	// std::vector<std::pair<std::string, std::string>> ps(n);
+	// for (auto& [s, t] : ps) {
+	// 	std::cin >> s >> t;
+	// 	buc.push_back(s);
+	// 	buc.push_back(t);
+	// }
+	// zawa::compression comp(buc);
+	// std::vector<std::vector<int>> G(comp.size());
+	// for (const auto& [s, t] : ps) {
+	// 	G[comp[s]].emplace_back(comp[t]);
+	// }
+	// std::cout << (zawa::topo_sort(G).ok() ? "Yes" : "No") << std::endl;
+	std::cout << "Hello World" << std::endl;
 }
 
 /*
