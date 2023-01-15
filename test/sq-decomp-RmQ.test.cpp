c4a16d192c18af3702006bba7926e1391f301d80
zawa-tin <contactaizawa@icloud.com>
2023-01-16 02:42:54 +0900

    182 sq-decomp verify

---
+++ test/sq-decomp-RmQ.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_A"
+
+#include "../src/dataStructure/sq-decomp.hpp"
+#include "../src/utility/monoid/min.hpp"
+
+#include <iostream>
+#include <vector>
+
+int main() {
+	int n, q; std::cin >> n >> q;
+	std::vector as(n, (int)((1LL << 31) - 1));
+	zawa::sq_decomp<zawa::min_monoid<int>> sq(as);
+	for (int _ = 0 ; _ < q ; _++) {
+		int com, x, y; std::cin >> com >> x >> y;
+		if (com == 0) {
+			as[x] = y;
+			sq.update(x, as[x]);
+		}
+		if (com == 1) {
+			std::cout << sq.prod(x, y + 1) << std::endl;
+		}
+	}
+}
