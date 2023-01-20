b484874d787af8c779c962ef366ba1be306e7c64
zawa-tin <contactaizawa@icloud.com>
2023-01-20 10:35:01 +0900

    verify accum2d #193

---
+++ test/ABC278-E.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "../src/dataStructure/accum2d.hpp"
+
+#include <iostream>
+#include <vector>
+
+int main() {
+	// int H, W, N, h, w; std::cin >> H >> W >> N >> h >> w;
+	// std::vector A(N, std::vector(H, std::vector(W, 0)));
+	// for (int i = 0 ; i < H ; i++) {
+	// 	for (int j = 0 ; j < W ; j++) {
+	// 		int a; std::cin >> a;
+	// 		A[a - 1][i][j]++;
+	// 	}
+	// }
+	// std::vector<zawa::accum2d<int>> S(N);
+	// for (int i = 0 ; i < N ; i++) {
+	// 	S[i] = zawa::accum2d(A[i]);
+	// }
+	// for (int i = 0 ; i + h <= H ; i++) {
+	// 	for (int j = 0 ; j + w <= W ; j++) {
+	// 		int ans = 0;
+	// 		for (int k = 0 ; k < N ; k++) {
+	// 			ans += (S[k].sum(0, 0, H, W) - S[k].sum(i, j, i + h, j + w) > 0);
+	// 		}
+	// 		std::cout << ans << (j + w == W ? '\n' : ' ');
+	// 	}
+	// }
+	std::cout << "Hello World" << std::endl;
+}
+
+/*
+ * AtCoder Beginner Contest 278 - E Grid Filling
+ * https://atcoder.jp/contests/abc278/submissions/38162869
+ */
