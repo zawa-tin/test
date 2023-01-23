306315c7cc3f816103e6d02d85a6fe9c3abb286e
zawa-tin <contactaizawa@icloud.com>
2023-01-23 11:10:29 +0900

    mobius function #202

---
+++ src/math/mobius-function.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "./prime-sieve.hpp"
+
+#include <vector>
+
+namespace zawa {
+
+class mobius_function {
+private:
+	std::vector<int> table;
+
+public:
+	mobius_function(std::size_t n) : table(std::vector(n + 1, 1)) {
+		prime_sieve siv(n);
+		for (std::size_t i = 2 ; i <= n ; i++) {
+			if (siv[i]) {
+				for (std::size_t j = i ; j <= n ; j += i) {
+					if (!(j % (i * i))) {
+						table[j] = 0;
+					}
+					else {
+						table[j] *= -1;
+					}
+				}
+			}
+		}
+	}
+
+	int operator[](int i) {
+		return table[i];
+	}
+};
+
+}// namespace zawa
---
+++ test/mobius-function.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "../src/math/mobius-function.hpp"
+
+#include <iostream>
+#include <cassert>
+
+int main() {
+	zawa::mobius_function mf(10);
+	std::vector ans = { 1, -1, -1, 0, -1, 1, -1, 0, 0, 1 };
+	for (int i = 0 ; i < 10 ; i++) {
+		assert(ans[i] == mf[i + 1]);
+	}
+	std::cout << "Hello World" << std::endl;
+}
