637b56b5fb7b79fe9f39eec63931bd78d2e38f9d
zawa-tin <contactaizawa@icloud.com>
2023-01-30 11:07:17 +0900

    fix verify #212

---
+++ test/ARC075-E.test.cpp
@@ -0,0 +1,32 @@
+#include "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "../src/dataStructure/fenwick_multiset.hpp"
+#include "../src/template/accum1d.hpp"
+#include "../src/algorithm/compression.hpp"
+
+#include <iostream>
+#include <vector>
+
+int main() {
+	// int N, K; std::cin >> N >> K;
+	// std::vector<long long> a(N);
+	// for (auto& ai : a) {
+	// 	std::cin >> ai;
+	// 	ai -= K;
+	// }
+	// zawa::accum1d S(a);
+	// zawa::compression comp(S);
+	// zawa::fenwick_multiset<int> set(comp.size());
+	// long long ans = 0LL;
+	// for (auto s : S) {
+	// 	ans += set.count(0, comp[s] + 1);
+	// 	set.insert(comp[s]);
+	// }
+	// std::cout << ans << std::endl;
+	std::cout << "Hello World" << std::endl;
+}
+
+/*
+ * AtCoder Regular Contest 075
+ * https://atcoder.jp/contests/arc075/submissions/38473188
+ */
