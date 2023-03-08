ee0f2f1c0ef8d5468731a9d36278ff7bb98d7ff7
zawa-tin <contactaizawa@icloud.com>
2023-03-08 19:51:16 +0900

    直線の平行、直交判定 #256

--- src/geometryR2/line.hpp
+++ src/geometryR2/line.hpp
@@ -2,15 +2,32 @@
 
 #include "./point.hpp"
 
+#include <cassert>
+
 namespace geoR2 {
 
 struct line {
 	point a, b;
+
 	line() : a(0, 0), b(0, 0) {}
 	line(const point& _a, const point& _b) : a(_a), b(_b) {}
-	bool isValid() const {
+
+	inline bool isValid() const {
 		return !equals(a, b);
 	}
+
 };
+
+bool isOrthogonal(const line& l1, const line& l2) {
+	assert(l1.isValid());
+	assert(l2.isValid());
+	return sgn(dot(l1.b - l1.a, l2.b - l2.a)) == 0;
+}
+
+bool isParallel(const line& l1, const line& l2) {
+	assert(l1.isValid());
+	assert(l2.isValid());
+	return sgn(cross(l1.b - l1.a, l2.b - l2.a)) == 0;
+}
 
 } // namespace geo2d
---
+++ test/AOJCGL-2A.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_A"
+
+#include "../src/geometryR2/line.hpp"
+
+#include <iostream>
+
+int main() {
+	using namespace geoR2;
+	int q; std::cin >> q;
+	for (int _ = 0 ; _ < q ; _++) {
+		line l1, l2;
+		std::cin >> l1.a >> l1.b >> l2.a >> l2.b;
+		if (isParallel(l1, l2)) {
+			std::cout << 2 << std::endl;
+		}
+		else if (isOrthogonal(l1, l2)) {
+			std::cout << 1 << std::endl;
+		}
+		else {
+			std::cout << 0 << std::endl;
+		}
+	}
+}
